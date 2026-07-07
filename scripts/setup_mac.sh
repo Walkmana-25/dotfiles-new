dc796951ecc58b25c75d2770306116ed98380b2c
Yuta Takahashi <y.highbridge04@gmail.com>
2026-07-08 01:04:04 +0900

    fix: add Homebrew installation check in macOS setup script

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -2,3 +2,9 @@
 
 echo "Setup Script For Darwin Based System"
 
+
+if ! command -v brew >/dev/null 2>&1; then
+    echo "Homebrew not found. Install Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
