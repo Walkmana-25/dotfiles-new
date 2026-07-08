31ad42087117f05f6eaa65af403fc2700e33830d
Yuta Takahashi <y.highbridge04@gmail.com>
2026-07-08 14:07:21 +0900

    feat: enhance macOS setup script with additional installation messages

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "--------------------------------------------------"
 echo "Setup Script For Darwin Based System"
+echo "--------------------------------------------------"
 
 
 if ! command -v brew >/dev/null 2>&1; then
@@ -8,3 +10,41 @@ if ! command -v brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+
+echo "--------------------------------------------------"
+echo "Install Brew Application"
+echo "--------------------------------------------------"
+brew update
+
+PACKAGES=(
+    git
+    jq
+    wget
+    tmux
+    gh
+)
+
+for pkg in "${PACKAGES[@]}"; do
+    if brew list "$pkg" &> /dev/null; then
+        echo "${pkg} is already installed."
+    else
+        echo "${pkg} is not installed. Installing..."
+        brew install "$pkg"
+    fi
+done
+
+echo "--------------------------------------------------"
+echo "Install Brew Cask Application"
+echo "--------------------------------------------------"
+
+PACKAGES_CASK=(
+)
+
+for pkg in "${PACKAGES_CASK[@]}"; do
+    if brew list "$pkg" &> /dev/null; then
+        echo "${pkg} is already installed."
+    else
+        echo "${pkg} is not installed. Installing..."
+        brew install --cask "$pkg"
+    fi
+done
